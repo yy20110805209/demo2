c2a206bdb19e3aed6b582408a43b408486f50665
deng yang <56860458+yy20110805209@users.noreply.github.com>
2022-07-26 10:31:34 +0800

    第一次提交

--- src/main/java/com/example1/demo2/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example1.demo2.mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example1.demo2.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-
-}
